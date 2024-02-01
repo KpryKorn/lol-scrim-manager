24ad89933193874a13ab9f5a7e81a1ec9d103450
KpryKorn <roffinisacha@gmail.com>
2024-02-01 14:52:43 +0100

    Add HomePlanning component and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import { HomePlanning } from "@/components/plannings";
+import { getWeek } from "@/lib/utils";
+
 export default function Home() {
+  let isConnected = true;
+
+  if (!isConnected) {
+    return (
+      <>
+        <div className="container">not connected</div>
+      </>
+    );
+  }
+
   return (
     <>
-      <div className="container">hello world</div>
+      <div className="container">
+        <h1 className="mb-4 text-4xl font-grotesk font-medium tracking-tighter">
+          Planning de scrims — 80ZETREI
+        </h1>
+        <p className="font-medium mb-4">{getWeek()}</p>
+        <HomePlanning />
+        <div>
+          <h2 className="underline underline-offset-4">
+            Objectifs de la semaine :
+          </h2>
+        </div>
+        <div>
+          <h3>Notes :</h3>
+        </div>
+      </div>
     </>
   );
 }
---
+++ components/plannings.tsx
@@ -0,0 +1,3 @@
+export function HomePlanning() {
+  return <div className="h-[450px] rounded-sm bg-gray-200 w-full"></div>;
+}
